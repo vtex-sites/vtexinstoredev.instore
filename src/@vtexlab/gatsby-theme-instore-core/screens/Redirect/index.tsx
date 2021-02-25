05725ae1ff8e449908ec3a02d26a48d69a00d9ac
Guilherme Bruzzi <guibruzzi@gmail.com>
2021-02-25 18:06:31 -0300

    Update plugins config

--- src/@vtexlab/gatsby-theme-instore-core/screens/Redirect/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import type { FC } from 'react'
-import { Link } from 'gatsby'
-
-const HomePage: FC = () => {
-  return <Link to="/instore">/instore</Link>
-}
-
-export default HomePage
