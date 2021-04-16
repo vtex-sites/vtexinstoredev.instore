d772a8240d90974a233450789433a53b6e9406f3
Guilherme Bruzzi <guibruzzi@gmail.com>
2021-04-16 17:59:06 -0300

    Update versions of the POC

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,9 @@ module.exports = {
       options: { tenant: STORE_ID },
     },
     {
+      resolve: '@vtexlab/gatsby-theme-instore-poc',
+    },
+    {
       resolve: '@vtex/gatsby-instore-plugin-example',
     },
     {
