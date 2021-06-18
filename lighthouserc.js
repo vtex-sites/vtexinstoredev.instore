f41a908534b73068b0cbe57f5eafdc85e779fc1c
gtkatakura <gt.katakura@gmail.com>
2021-06-18 10:42:08 -0300

    chore(lighthouse): lower assertions to test renovate automerge

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,4 +2,13 @@ const VTEXLHConfig = require('@vtex/lighthouse-config').default
 
 const urls = ['', '/instore/login']
 
-module.exports = VTEXLHConfig({ urls, server: process.env.BASE_SITE_URL })
+module.exports = VTEXLHConfig({
+  urls,
+  server: process.env.BASE_SITE_URL,
+  assertions: {
+    'categories:accessibility': ['error', { minScore: 0.9 }],
+    'categories:best-practices': ['error', { minScore: 0.9 }],
+    'categories:performance': ['error', { minScore: 0.9 }],
+    'first-contentful-paint': ['error', { maxNumericValue: 3000 }],
+  },
+})
