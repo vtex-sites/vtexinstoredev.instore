9b98259c5511c70ed8ab65bea89a92b992703271
Guilherme Bruzzi <guibruzzi@gmail.com>
2022-01-20 15:04:10 -0300

    Try to make same config as vtexgame1 to see if it builds

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,9 @@ module.exports = {
       resolve: '@vtexlab/gatsby-theme-instore-core',
       options: { tenant: STORE_ID },
     },
+    {
+      resolve: 'gatsby-plugin-no-sourcemaps',
+    },
     // {
     //   resolve: '@vtexlab/gatsby-theme-instore-poc',
     // },
