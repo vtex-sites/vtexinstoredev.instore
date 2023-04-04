5df6e5827c1132c391f0efd618efe9edae54f611
Guilherme Lacerda <guilherme.lcd@gmail.com>
2023-04-04 15:53:11 -0300

    chore: add cms config files

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Page() {
+  return <>Hello World</>
+}
+
+export default Page
---
+++ store.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  platform: 'vtex',
+  api: {
+    storeId: 'vtexinstoredev',
+    environment: 'vtexcommercestable',
+  },
+}
